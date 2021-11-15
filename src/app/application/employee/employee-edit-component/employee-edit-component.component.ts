aab99195f4db4d0e537801995dcaa9cc48fe3295
Ignacio Vacca <vaccaignacio@gmail.com>
2021-11-15 18:19:06 -0300

    With scss

--- src/app/application/employee/employee-edit-component/employee-edit-component.component.ts
+++ src/app/application/employee/employee-edit-component/employee-edit-component.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-employee-edit-component',
   templateUrl: './employee-edit-component.component.html',
-  styleUrls: ['./employee-edit-component.component.css']
+  styleUrls: ['./employee-edit-component.component.scss']
 })
 export class EmployeeEditComponentComponent implements OnInit {
 
--- src/app/application/employee/employee-list-component/employee-list-component.component.ts
+++ src/app/application/employee/employee-list-component/employee-list-component.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-employee-list-component',
   templateUrl: './employee-list-component.component.html',
-  styleUrls: ['./employee-list-component.component.css']
+  styleUrls: ['./employee-list-component.component.scss']
 })
 export class EmployeeListComponentComponent implements OnInit {
 
