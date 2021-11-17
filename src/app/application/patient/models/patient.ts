b9bcf98e043a647873e39fa13977ec641c059452
Ignacio Vacca <vaccaignacio@gmail.com>
2021-11-16 21:19:51 -0300

    Create or edit patient

--- src/app/application/patient/models/patient.ts
+++ src/app/application/patient/models/patient.ts
@@ -1,7 +1,19 @@
+import { HealthInsurance } from "../../health-insurance/models/health-insurance";
+import { Contact } from "./contact";
+
 export class Patient {
     id?: number;
     firstName: string;
     lastName: string;
     birthDate: Date;
     dni: number;
+    contact?:Contact;
+    healthInsurance?:HealthInsurance;
+    afiliateNumber:number;
+    clinicHistory:string;
+
+    constructor() {
+        this.healthInsurance = new HealthInsurance();
+        this.contact = new Contact();
+    }
 }
--- src/app/application/patient/patient-edit/patient-edit.component.ts
+++ src/app/application/patient/patient-edit/patient-edit.component.ts
@@ -1,4 +1,13 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators, AbstractControl, ValidatorFn } from '@angular/forms';
+import { MatDialogConfig, MatDialog } from '@angular/material/dialog';
+import { Router, ActivatedRoute } from '@angular/router';
+import { SuccessDialogComponent } from 'src/app/shared/dialogs/success-dialog/success-dialog.component';
+import { ErrorHandlerService } from 'src/app/shared/services/error-handler.service';
+import { HealthInsurance } from '../../health-insurance/models/health-insurance';
+import { HealthInsuranceService } from '../../health-insurance/services/health-insurance.service';
+import { Patient } from '../models/patient';
+import { PatientService } from '../services/patient.service';
 
 @Component({
   selector: 'app-patient-edit',
@@ -7,9 +16,103 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PatientEditComponent implements OnInit {
 
-  constructor() { }
+
+  public patientForm: FormGroup;
+  public selectedPatient: Patient = new Patient();
+  private dialogConfig: MatDialogConfig;
+  public healthInsurances: Array<HealthInsurance> = new Array<HealthInsurance>();
+  loading = false;
+
+  constructor(private router: Router, private errorService: ErrorHandlerService, private patientService: PatientService, private healthInsuranceService: HealthInsuranceService, private activeRoute: ActivatedRoute, private dialog: MatDialog) { }
 
   ngOnInit(): void {
+    this.getPatient();
+    this.getHealthInsurances();
+    this.patientForm = new FormGroup({
+      firstName: new FormControl(this.selectedPatient.firstName, [Validators.required, Validators.maxLength(60)]),
+      lastName: new FormControl(this.selectedPatient.lastName, [Validators.required, Validators.maxLength(60)]),
+      dni: new FormControl(this.selectedPatient.dni, [Validators.required, Validators.maxLength(60)]),
+      birthDate: new FormControl(this.selectedPatient.birthDate, [Validators.required, this.dateValidator()]),
+      healthInsurance: new FormControl(this.selectedPatient.healthInsurance.id, [Validators.required]),
+      afiliateNumber: new FormControl(this.selectedPatient.afiliateNumber),
+      clinicHistory: new FormControl(this.selectedPatient.clinicHistory, [Validators.required]),
+      
+    });
+    this.dialogConfig = {
+      height: '300px',
+      width: '400px',
+      disableClose: true,
+      data: {}
+    }
   }
+
+  private getPatient(): void {
+    let id: number = this.activeRoute.snapshot.params['id'];
+    if (id !== undefined) {
+      this.patientService.get(id).subscribe((data) => this.selectedPatient = data);
+    }
+  }
+
+  private getHealthInsurances(): void {
+    this.healthInsuranceService.getAll().subscribe((data) => this.healthInsurances = data);
+  }
+
+  updatePatient() {
+    if (!this.patientForm.valid)
+      return;
+    console.log(this.selectedPatient);
+    if (this.selectedPatient.id !== undefined) {
+      this.patientService.update(this.selectedPatient).subscribe((data) => {
+        let dialogRef = this.dialog.open(SuccessDialogComponent, this.dialogConfig);
+        dialogRef.afterClosed()
+          .subscribe(result => {
+            this.router.navigate(['/app/patient/list']);
+          });
+      }, (error => {
+        this.errorService.dialogConfig = { ...this.dialogConfig };
+        this.errorService.handleError(error);
+      }));
+    } else {
+      this.patientService.create(this.selectedPatient).subscribe((data) => {
+        let dialogRef = this.dialog.open(SuccessDialogComponent, this.dialogConfig);
+        dialogRef.afterClosed()
+          .subscribe(result => {
+            this.router.navigate(['/app/patient/list']);
+          });
+      }, (error => {
+        this.errorService.dialogConfig = { ...this.dialogConfig };
+        this.errorService.handleError(error);
+      }));
+    }
+  }
+
+  editPatient(selectedPatient: Patient) {
+    this.selectedPatient = selectedPatient;
+  }
+
+  clearPatient() {
+    this.selectedPatient = new Patient();
+  }
+
+  public hasError(controlName: string, errorName: string): boolean {
+    return this.patientForm.controls[controlName].hasError(errorName);
+  }
+
+  public dateValidator(): ValidatorFn {
+    return (control: AbstractControl): { [key: string]: any } | null => {
+      const today = new Date().getTime();
+
+      if (!(control && control.value)) {
+        // if there's no control or no value, that's ok
+        return null;
+      }
+
+      // return null if there's no errors
+      return control.value.getTime() > today
+        ? { invalidDate: 'No se puede elegir una fecha posterior a hoy' }
+        : null;
+    }
+  }
+
 
 }
