4e9e33cdfac69d8c8ea90b06604eea14babd7b7f
Ignacio Vacca <vaccaignacio@gmail.com>
2021-12-26 15:46:35 -0300

    Fix harcoded date and list study with status

--- src/app/application/study/models/study-item.ts
+++ src/app/application/study/models/study-item.ts
@@ -1,6 +1,7 @@
 import { Patient } from "../../patient/models/patient";
 import { ReferringDoctor } from "../../referring-doctor/models/referring-doctor";
 import { PresumptiveDiagnosis } from "./presumptive-diagnosis";
+import { StudyStatus } from "./study-status";
 import { StudyType } from "./study-type";
 
 export class StudyItem {
@@ -12,10 +13,12 @@ export class StudyItem {
     extractionAmount: number;
     studyType: StudyType;
     presumptiveDiagnosis: PresumptiveDiagnosis;
+    actualStatus: StudyStatus;
 
     constructor(){
         this.studyType = new StudyType();
         this.presumptiveDiagnosis = new PresumptiveDiagnosis();
+        this.actualStatus = new StudyStatus();
         this.budget = 0.0;
         this.extractionAmount = 0.0;
     }
