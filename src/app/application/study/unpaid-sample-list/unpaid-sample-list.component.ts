0035e4e089a37da61fb6d709272eea923825a1e8
Ignacio Vacca <vaccaignacio@gmail.com>
2021-12-28 21:18:51 -0300

    [WIP] Init unpaid sample list

---
+++ src/app/application/study/unpaid-sample-list/unpaid-sample-list.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-unpaid-sample-list',
+  templateUrl: './unpaid-sample-list.component.html',
+  styleUrls: ['./unpaid-sample-list.component.scss']
+})
+export class UnpaidSampleListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
